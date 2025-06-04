7ff087c86a526ee093b5aff1949bfa7463bb2085
SavaKufarov <sava.kufarov@gmail.com>
2025-06-04 16:05:28 +0300

    Updating the view. Adding modern html and css design for home page and sign up page.

--- GrupovProektRS1/Controllers/HomeController.cs
+++ GrupovProektRS1/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace GrupovProektRS1.Controllers
         {
             return View();
         }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
